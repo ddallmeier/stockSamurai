5cb1cb1a450a194fe500bdcaa96cefee885aa0f6
ddallmeier <jdd158@miami.edu>
2022-11-03 10:33:30 -0400

    added basic search bars, updated readme, fixed err

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+import plotly.graph_objects as go
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output
+import pandas as pd
 
-# msft.sustainability
+app = Dash(__name__)
 
-data = yf.download(  # or pdr.get_data_yahoo(...
-        # tickers list or string as well
-        tickers = "SPY AAPL MSFT",
 
-        # use "period" instead of start/end
-        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-        # (optional, default is '1mo')
-        period = "ytd",
+## Data selection 
+df = yf.download('AAPL', start='2019-01-01', end='2020-01-01')
+print(df)
 
-        # fetch data by interval (including intraday if period < 60 days)
-        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-        # (optional, default is '1d')
-        interval = "5m",
 
-        # group by ticker (to access via data['SPY'])
-        # (optional, default is 'column')
-        group_by = 'ticker',
+## Input search bar
+ALLOWED_TYPES = (
+    "ticker", "number", "password", "email", "search",
+    "tel", "url", "range", "hidden",
+)
 
-        # adjust all OHLC automatically
-        # (optional, default is False)
-        auto_adjust = True,
+app.layout = html.Div(
+    [
+        dcc.Input(
+            id="input_{}".format(_),
+            type=_,
+            placeholder="input type {}".format(_),
+        )
+        for _ in ALLOWED_TYPES
+    ]
+    + [html.Div(id="out-all-types")]
+)
 
-        # download pre/post regular market hours data
-        # (optional, default is False)
-        prepost = True,
 
-        # use threads for mass downloading? (True/False/Integer)
-        # (optional, default is True)
-        threads = True,
+@app.callback(
+    Output("out-all-types", "children"),
+    [Input("input_{}".format(_), "value") for _ in ALLOWED_TYPES],
+)
+def cb_render(*vals):
+    return " | ".join((str(val) for val in vals if val))
 
-        # proxy URL scheme use use when downloading?
-        # (optional, default is None)
-        proxy = None
-    )
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
